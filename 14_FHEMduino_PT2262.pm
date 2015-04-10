d56ce37f43c43120432a3a2f9614cbc9fdf7f7b3
hjgode <hjgode@gmail.com>
2015-04-10 20:17:26 +0200

    added off-till

--- 14_FHEMduino_PT2262.pm
+++ 14_FHEMduino_PT2262.pm
@@ -2,6 +2,7 @@
 # FHEMduino PT2262 Modul (Remote/Switch)
 # $Id: 14_FHEMduino_PT2262.pm 0002 2014-05-28 snoop & mdorenka $
 # 2014-06-15: SetExtension und on-till und on-for-timer implementiert
+# 2015-04-10: added off-till, hjgode
 
 package main;
 
@@ -77,6 +78,31 @@ FHEMduino_PT2262_Do_On_Till($@)
   my $tname = $hash->{NAME} . "_till";
   CommandDelete(undef, $tname) if($defs{$tname});
   CommandDefine(undef, "$tname at $hms_till set $a[0] off");
+
+}
+
+sub
+FHEMduino_PT2262_Do_Off_Till($@)
+{
+  my ($hash, @a) = @_;
+  return "Timespec (HH:MM[:SS]) needed for the off-till command" if(@a != 3);
+
+  my ($err, $hr, $min, $sec, $fn) = GetTimeSpec($a[2]);
+  return $err if($err);
+
+  my @lt = localtime;
+  my $hms_till = sprintf("%02d:%02d:%02d", $hr, $min, $sec);
+  my $hms_now = sprintf("%02d:%02d:%02d", $lt[2], $lt[1], $lt[0]);
+  if($hms_now ge $hms_till) {
+    Log 4, "off-till: won't switch as now ($hms_now) is later than $hms_till";
+    return "";
+  }
+
+  my @b = ($a[0], "off");
+  FHEMduino_PT2262_Set($hash, @b);
+  my $tname = $hash->{NAME} . "_till";
+  CommandDelete(undef, $tname) if($defs{$tname});
+  CommandDefine(undef, "$tname at $hms_till set $a[0] on");
 
 }
 
@@ -184,8 +210,8 @@ sub FHEMduino_PT2262_Set($@){ ##################################################
   return "no set value specified" if($na < 2 || $na > 3);
   
   my $list = "";
-  $list .= "off:noArg on:noArg on-till on-for-timer"; # if( AttrVal($hname, "model", "") ne "itremote" );
-  $list .= "dimUp:noArg dimDown:noArg on-till" if( AttrVal($hname, "model", "") eq "itdimmer" );
+  $list .= "off:noArg on:noArg on-till off-till on-for-timer"; # if( AttrVal($hname, "model", "") ne "itremote" );
+  $list .= "dimUp:noArg dimDown:noArg on-till off-till" if( AttrVal($hname, "model", "") eq "itdimmer" );
 
   return SetExtensions($hash, $list, $hname, @a) if( $a[1] eq "?" );
   return SetExtensions($hash, $list, $hname, @a) if( !grep( $_ =~ /^$a[1]($|:)/, split( ' ', $list ) ) );
@@ -193,6 +219,7 @@ sub FHEMduino_PT2262_Set($@){ ##################################################
   my $c = $elro_c2b{$a[1]};
 
   return FHEMduino_PT2262_Do_On_Till($hash, @a) if($a[1] eq "on-till");
+  return FHEMduino_PT2262_Do_Off_Till($hash, @a) if($a[1] eq "off-till");
   return "Bad time spec" if($na == 3 && $a[2] !~ m/^\d*\.?\d+$/);
 
   return FHEMduino_PT2262_On_For_Timer($hash, @a) if($a[1] eq "on-for-timer");
